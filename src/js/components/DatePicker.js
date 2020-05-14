c060f8f307b12ef75ffbeb5fc29d0699006e92c7
Michał <michal.bootcamp@gmail.com>
2020-05-15 00:06:00 +0200

    use single date string instead of dates array

--- src/js/components/DatePicker.js
+++ src/js/components/DatePicker.js
@@ -10,16 +10,16 @@ export class DatePicker extends BaseWidget {
   constructor(wrapper){
     super(wrapper, utils.dateToStr(new Date()));
     const thisWidget = this;
-    
+
     thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.datePicker.input);
     //console.log(thisWidget.dom.input);
-    
+
     thisWidget.initPlugin();
   }
 
-  initPlugin() { //app. komp., która rozszerza funkcjon. przgłądarki 
+  initPlugin() { //app. komp., która rozszerza funkcjon. przgłądarki
     const thisWidget = this;
-    
+
     thisWidget.minDate = new Date(thisWidget.value);  // tworzy obiekt daty, którego wartość to "teraz", czyli data i godzina w momencie wykonania tego kodu JS.
     thisWidget.maxDate = utils.addDays(thisWidget.minDate, settings.datePicker.maxDaysInFuture);
     //aby uzyskać datę przesuniętą o ileś dni,
@@ -37,7 +37,8 @@ export class DatePicker extends BaseWidget {
       locale: {
         firstDayOfWeek: 1, // start week on Monday
       },
-      onChange: function (dateStr) {
+      onChange: function (selectedDates, dateStr) {
+        console.log(dateStr);
         thisWidget.value = dateStr;
       },
     });
