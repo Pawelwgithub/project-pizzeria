ff2a776bfd570a02bfd662a2f0ed55d0e3c54886
Paweł Walkowski <walkowski.pawel@wp.pl>
2020-05-28 16:04:21 +0200

    Module 10.4. Publication of the project on Heroku

--- src/js/settings.js
+++ src/js/settings.js
@@ -118,7 +118,8 @@ export const settings = {
     tableIdAttribute: 'data-table',
   },
   db: {
-    url: '//localhost:3131',
+    //url: '//localhost:3131',
+    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''), // Module 10.4
     product: 'product',
     order: 'order',
     booking: 'booking',
